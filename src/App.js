63772f84b660a401e924b3131d0db097d0ceac8b
Thien Nguyen <anthien30@gmail.com>
2021-01-31 17:47:25 -0800

    added HomePage.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Canvas from './components/Canvas.js'
+import logo from "./logo.svg";
+import "./App.css";
+import Canvas from "./components/Canvas.js";
+import LoginPage from "./components/LoginPage";
+import RandomName from "./components/LoginPage";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Canvas/>
+        {/* <Canvas /> */}
+        <LoginPage />
       </header>
     </div>
   );
